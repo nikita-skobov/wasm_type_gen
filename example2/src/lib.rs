536fa9337f53ebc073073c60dd82c19a35cea354
nikita-skobov <nikita@skobov.io>
2023-04-21 20:06:13 -0600

    adds docs for example2

--- example2/src/lib.rs
+++ example2/src/lib.rs
@@ -1,5 +1,17 @@
+///! Instructions:
+///! ```
+///! cd example2/
+///! mkdir -p wasm_modules
+///! cp ./src/modtest.rs ./wasm_modules/
+///! cargo build
+///! ```
+///! Edit this file, and try to change obj.apples to anything but 2.
+///! Then look at src/modtest.rs to see why it works.
+///! Every time you change modtest.rs you must copy it over to ./wasm_modules/
+
 use example2_derive::{wasm_meta, wasm_modules};
 
+// This macro expects to find a mymod.rs file in ./wasm_modules/
 wasm_modules!("mymod.rs");
 
 mod modtest;
