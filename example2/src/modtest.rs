1d95fbfe9237b5a96cfa01526c03f3907fd989ba
nikita-skobov <nikita@skobov.io>
2023-04-21 19:56:31 -0600

    no warnings

--- example2/src/modtest.rs
+++ example2/src/modtest.rs
@@ -3,13 +3,15 @@
 ///! This just inlines the types that wasm_module writers care about such as LibraryObj, and UserData.
 ///! After writing this file w/ type hints, you would copy it over to your `wasm_modules/` directory
 ///! in order to use it as a wasm module.
-
+///! Note: we use #[allow(dead_code)] everywhere below! This is because the code is unused from the main program, but
+///! rather is compiled separately. This is not necessary, but it avoids warnings when editing which is nice
 use example2_derive::wasm_meta;
 #[wasm_meta]
 const _: () = ();
 // everything below this line can be copied into wasm_modules:
 
 /// doc comments work!
+#[allow(dead_code)]
 pub struct MyStruct {
     /// this value can only be 2!
     /// this is a simple example, but it shows
@@ -17,8 +19,10 @@ pub struct MyStruct {
     pub apples: u32,
 }
 
+#[allow(dead_code)]
 pub type ExportType = MyStruct;
 
+#[allow(dead_code)]
 pub fn wasm_entrypoint(obj: &mut LibraryObj, cb: fn(&mut MyStruct)) {
     let mut mystuff = MyStruct {
         apples: 2,
